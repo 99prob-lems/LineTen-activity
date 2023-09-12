1b285dd0ae5bf3f090f96a6161ffe6af14e10d33
Yande <yande@Yandes-MacBook-Pro.local>
2023-09-12 14:43:12 +0100

    first layout wip

--- cypress/e2e/features/lineTen.feature
+++ cypress/e2e/features/lineTen.feature
@@ -1,4 +1,21 @@
 Feature: LineTen services test
 
+    Background: BaseUrl
+        Given I have baseurl 'https://linetencustomerapi.azurewebsites.net/'
+
     Scenario Outline: Check out some stuff
-        When I am trying something
+        And I have the following request payload:
+            | key     | value        |
+            | Email   | john@doe.com |
+            | Phone   | 07499111222  |
+            | Address | 191 wood ln  |
+            | City    | London       |
+            | Zip     | W12 7FP      |
+            | Country | UK           |
+            | Notes   | Hello        |
+        # When I submit the request to endpoint ''
+
+
+
+
+
--- cypress/e2e/step_definitions/lineTen.step.js
+++ cypress/e2e/step_definitions/lineTen.step.js
@@ -6,6 +6,32 @@ import {
     Before
   } from "@badeball/cypress-cucumber-preprocessor";
 
-  Given("I am trying something", () => {
-    cy.log('test me')
+  const payload = {}; // can clean up later, or maybe store in cypress.env
+  const baseUrl = '';
+
+  Given('I have baseurl {string }', (base) => {
+    baseUrl = base;
   });
+
+  Given('I have the following request payload:', (dataTable) => {
+    payload = {
+      Email: null,
+      Phone: null,
+      Address: null,
+      City: null,
+      Zip: null,
+      Country: null,
+      Notes: null
+    }
+    dataTable.hashes().forEach(row =>{
+      if (row.key in payload) {
+        payload[row.key] = row.value
+      }
+    })
+  });
+
+  Given('I submit the request to endpoint {string}', (endPoint) => {
+    cy.log(baseUrl+endPoint)
+  });
+
+  
